b69a30f732a423bbdc9f37efffb786e3f4f6ee4d
jwolff52 <james.wolff0@gmail.com>
2014-02-07 00:36:39 -0600

    Huge Code overhaul

--- src/chapplication/util/CServerUtilities.java
+++ src/chapplication/util/CServerUtilities.java
@@ -1,26 +1,57 @@
 package chapplication.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author james.wolff
  * @date Sep 18, 2013
  */
 public class CServerUtilities implements CUtilities{
-
+    private static Connection conn;
+    private static Statement stmt;
+    private static String tblName;
+    private static ArrayList<String[]> userInfo;
+    public CServerUtilities(String table){
+        initSeverConnection();
+        initUserInfo();
+        tblName=table;
+    }
     @Override
     public boolean usernameExsists(String user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt=conn.createStatement();
+            ResultSet result=stmt.executeQuery("SELECT userID FROM "+tblName+" WHERE userID EQUALS "+user);
+            stmt.close();
+            return result.getString("userID").equals(user);
+        } catch (SQLException ex) {
+            Logger.getLogger(CServerUtilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
     public boolean passwordExsists(String pass) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt=conn.createStatement();
+            ResultSet result=stmt.executeQuery("SELECT password FROM "+tblName+" WHERE password EQUALS "+pass);
+            stmt.close();
+            return result.getString("password").equals(pass);
+        } catch (SQLException ex) {
+            Logger.getLogger(CServerUtilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
     public ArrayList getUserInfo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userInfo;
     }
 
     @Override
@@ -43,4 +74,29 @@ public class CServerUtilities implements CUtilities{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    private void initSeverConnection() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            conn=DriverManager.getConnection("jdbc:derby://localhost:1527/chapplication;", "root_admin", "thisisareallylongpassword");
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(CServerUtilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void initUserInfo(){
+        try {
+            userInfo=new ArrayList<>();
+            stmt=conn.createStatement();
+            ResultSet result=stmt.executeQuery("select userID from ROOT_ADMIN.USERS");
+            String[] sa=new String[2];
+            while(result.next()){
+                sa[0]=result.getString(2);
+                sa[1]=result.getString(3);
+                userInfo.add(sa);
+            }
+            stmt.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(CServerUtilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
